ad65ece44b045f51acaef4bdad449c4e59ec5cc8
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2024-12-11 14:31:31 +0530

    create new layout for admin

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Stops",
+    icon: "pin_drop",
+    link: "/admin/stops",
+  },
+  {
+    title: "Routes",
+    icon: "directions",
+    link: "/admin/routes",
+  },
+  {
+    title: "Vehicles",
+    icon: "directions_bus",
+    link: "/admin/vehicles",
+  },
+  {
+    title: "Drivers",
+    icon: "person",
+    link: "/admin/drivers",
+  },
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
